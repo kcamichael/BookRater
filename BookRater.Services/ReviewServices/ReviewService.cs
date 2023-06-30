f1853cd5a6de0f0f0ec581dd33515ee6af59797b
ouiza chibane <ouizachibanee@gmail.com>
2023-06-29 20:09:22 -0400

    louize-build

--- BookRater.Services/ReviewServices/ReviewService.cs
+++ BookRater.Services/ReviewServices/ReviewService.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookRater.Data.BookRaterContext;
 
 namespace BookRater.Services.ReviewServices
 {
-    public class ReviewService
+    public class ReviewService : IReviewService
     {
-        
+        private readonly BookRaterDBContext _context;
+        private readonly IMapper _mapper;
+
+        public ReviewService(BookRaterDBContext context, IMapper mapper)
+        {
+            
+            
+        }
+
+
+
     }
 }
