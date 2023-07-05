e8331b8b034cb39f61808f8d17f8e4f5e4128835
Clarissa Fields <clarissa.alaze@gmail.com>
2023-07-05 18:25:41 -0400

    AuthResoponse.cs created and updated

---
+++ BookRater.API/Conttrollers/Accountscontroller.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookRater.API.Conttrollers
+{
+    public class Accountscontroller : ControllerBase
+    {
+        private readonly IAuthenticationManager _authManager;
+
+    }
+}
